098d6b5b9718c65cd583de4bdc43e14533c0222f
RodolfoLevill <rodolfo.levill@gmail.com>
2022-07-07 14:59:09 -0400

    Identificacion de TC's

--- Test/card.test.js
+++ Test/card.test.js
@@ -9,14 +9,14 @@ describe('Using first list', () => {
         idFirst = await listPage.getFirstList();
     });
 
-    test('should get cards list', async () => {
+    test('[TC-0006]Should get cards list', async () => {
         const response = await cardPage.getCardList();
         expect(response.status).toBe(200);
         expect(response.data.name).toEqual('My first board');
 
     });
 
-    test('should Create Card in the first List', async () => {
+    test('[TC-0007]Should Create Card in the first List', async () => {
         const cardName = ("Card Number: " + parseInt(Math.random() * 10000));
         const response = await cardPage.postCreateCard(idFirst, cardName);
 
--- Test/list.test.js
+++ Test/list.test.js
@@ -11,18 +11,18 @@ describe('Trello API testing', () => {
             idLast = await listPage.getLastList();
         });
 
-        test('Create List with random name ', async () => {
+        test('[TC-0001]Create List with random name ', async () => {
             const listName = ("List Number: " + parseInt(Math.random() * 10000));
             const response = await listPage.postCreateList(listName);
             expect(response.status).toBe(200);
             expect(response.data.name).toEqual(listName);
         });
 
-        test('should Get first list in the Board', async () => {
+        test('[TC-0002]Should Get first list in the Board', async () => {
             expect(idFirst).toBeDefined();
         });
 
-        test('should Updated List Name', async () => {
+        test('[TC-0003]Should Updated List Name', async () => {
             const newName = ("Name is update here: " + parseInt(Math.random() * 10000));
             const response = await listPage.putUpdateListName(idFirst,newName);
 
@@ -30,11 +30,11 @@ describe('Trello API testing', () => {
             expect(response.data.name).toEqual(newName);
         });
 
-        test('should Get last list in the Board', () => {
+        test('[TC-0004]Should Get last list in the Board', () => {
             expect(idLast).toBeDefined();
         });
 
-        test('should Delete last List', async () => {
+        test('[TC-0005]Should Delete last List', async () => {
             const response = await listPage.putDeleteLastList(idLast);
             expect(response.status).toBe(200);
         });
